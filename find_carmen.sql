aa1fab79e93f8a3f4ddf1203dc7be1f1bcfc2d46
Alya Aldaka <alyaaldaka08@gmail.com>
2026-09-20 21:52:04 +0300

    anotherr one

--- find_carmen.sql
+++ find_carmen.sql
@@ -43,7 +43,9 @@ WHERE ci.name LIKE 'Serra%' AND c.continent = 'South America';
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
  -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
  -- follow right behind you!
-
+SELECT ci.name FROM city ci JOIN country c ON ci.countrycode = c.code
+WHERE c.capital = ci.id AND c.name = 'Brazil';
+--Brasï¿½lia wherever this is, upon research its Brasília but the latin char didnt work
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
