bf78ce0999cec6230f2255a16d079727c56a4084
Alya Aldaka <alyaaldaka08@gmail.com>
2026-09-20 21:39:30 +0300

    another one

--- find_carmen.sql
+++ find_carmen.sql
@@ -16,14 +16,20 @@ WHERE country.name = 'Holy See (Vatican City State)' AND countrylanguage.isoffic
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
 --  nearby country speaks nothing but that language.
-
+SELECT country.name FROM country JOIN countrylanguage ON countrylanguage.countrycode = country.code
+WHERE countrylanguage.language = 'Italian' AND countrylanguage.percentage = 100;
+--San Marino
 
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time.
  -- There are only two cities she could be flying to in the country. One is named the same as the country – that
  -- would be too obvious. We're following our gut on this one; find out what other city in that country she might
  --  be flying to.
-
+ SELECT ci.name FROM city ci JOIN country c ON  ci.countrycode = c.code
+WHERE c.name = 'San Marino' AND ci.name != 'San Marino';
+-- [another solution] SELECT ci.name FROM city ci JOIN country c ON  ci.countrycode = c.code
+--WHERE c.name = 'San Marino' AND ci.name != c.name;
+--Serravalle
 
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
