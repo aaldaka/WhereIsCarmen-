65eb97c7a4ab30371c2795f0ab4092ef9ce3cdca
Alya Aldaka <alyaaldaka08@gmail.com>
2026-09-20 21:30:26 +0300

    another clue

--- find_carmen.sql
+++ find_carmen.sql
@@ -3,14 +3,15 @@
 -- so find the least populated country in Southern Europe, and we'll start looking for her there.
 SELECT name, population FROM country WHERE region = 'Southern Europe'ORDER BY population ASC
 LIMIT 1;
-
+-- Holy See (Vatican City State) the answer
 
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
-
-
+SELECT countrylanguage.language FROM countrylanguage JOIN country ON countrylanguage.countrycode = country.code
+WHERE country.name = 'Holy See (Vatican City State)' AND countrylanguage.isofficial = true;
+-- Italian (thats new ngl)
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
 -- to a different country, a country where people speak only the language she was learning. Find out which
