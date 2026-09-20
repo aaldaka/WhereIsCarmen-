e9feaba19845a9ee3c108429365916fba1909f6f
Alya Aldaka <alyaaldaka08@gmail.com>
2026-09-20 21:45:17 +0300

    anotha one

--- find_carmen.sql
+++ find_carmen.sql
@@ -35,7 +35,9 @@ WHERE c.name = 'San Marino' AND ci.name != 'San Marino';
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
-
+SELECT c.name, ci.name FROM country c JOIN city ci ON  c.code = ci.countrycode
+WHERE ci.name LIKE 'Serra%' AND c.continent = 'South America';
+-- Serra, Brazilll
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
